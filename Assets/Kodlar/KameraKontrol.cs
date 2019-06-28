b0562e86a74349e3ebf9afa3027d70b01034a261
furser <furkann.serdaroglu@gmail.com>
2019-06-28 18:37:55 +0300

    RollABall

---
+++ Assets/Kodlar/KameraKontrol.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KameraKontrol : MonoBehaviour {
+
+    public GameObject top;
+    Vector3 aradakiMesafe;
+
+	// Use this for initialization
+	void Start () {
+
+        aradakiMesafe = transform.position - top.transform.position;
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        
+        transform.position = top.transform.position + aradakiMesafe;
+		
+	}
+}
---
+++ Assets/Kodlar/TopKontrol.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TopKontrol : MonoBehaviour {
+
+    Rigidbody fizik;
+    public int hiz;
+    int sayac;
+    public int toplamMat;
+    public Text sayacText;
+    public Text oyunBitti;
+
+    // Use this for initialization
+    void Start()
+    {
+       
+        fizik = GetComponent<Rigidbody>();
+        sayac = 0;
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float yatay = Input.GetAxisRaw("Horizontal");
+        float dikey = Input.GetAxisRaw("Vertical");
+
+        Vector3 vec = new Vector3(yatay, 0, dikey);
+
+        fizik.AddForce(vec*hiz);
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "engel")
+        {
+            Destroy(other.gameObject);
+            sayac++;
+
+            sayacText.text = "Score : " + sayac;
+
+            if (sayac == toplamMat)
+            {
+                oyunBitti.text = "Game Over!";
+            }
+        }
+
+    }
+}
